529b125877cd6313221b4a980ff6679cbf749daf
Vishvambruth JavagalThimmegowda <vishruth.cse@gmail.com>
2021-06-10 22:18:03 +0530

    Refined Code

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
 python -m venv venv
 source venv/Scripts/activate
 pip install -r requirements.txt
-py.test -s -v --html=reports/report.html --capture=tee-sys
+py.test -s -v --html=reports/report.html --capture=tee-sys --junitxml="reports/result.xml"
